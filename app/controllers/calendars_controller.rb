f2cbfd5572210c254868a32f908a21da9a0dc348
marfy0120 <marfy0528@icloud.com>
2020-11-21 18:37:13 +0900

    Revert "プルリクエストし直し"
    This reverts commit 1d5f82b3f922857709030946e2f72ccec0439fac.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    # binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -16,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -26,25 +25,16 @@ class CalendarsController < ApplicationController
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
-
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
+
     7.times do |x|
       today_plans = []
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-
-      wday_num = Date.today.wday + x
-      if 
-        wday_num = wday_num -7
-      end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays [wday_num]}
-
-    
-
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
